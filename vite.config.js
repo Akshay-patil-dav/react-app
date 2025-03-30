4b525c2caa4d6f617226ebc1b4b50474df5846dd
Akshay Patil <akshaypatil22082002@gmail.com>
2025-03-30 20:23:37 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://akshay-patil-dav.github.io/react-app/', // Set '/' if deployed at the domain root
 })
